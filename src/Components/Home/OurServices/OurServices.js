87d86ba294d48422930d72603099abb99749a1fc
NEYAZ NAFIZ <niiyazobug@gmail.com>
2022-07-20 23:13:19 +0600

    our service cart added

--- src/Components/Home/OurServices/OurServices.js
+++ src/Components/Home/OurServices/OurServices.js
@@ -1,18 +1,19 @@
 import React from 'react'
+import './OurServices.css'
 
 function OurServices() {
     return (
-        <div className='grid justify-center my-10'>
+        <div className='grid justify-center my-10 mb-10 pb-10'>
             <div className="grid grid-cols-3 gap-5">
+
                 <div className="mb-4">
                     <div className="relative overflow-hidden bg-no-repeat bg-cover max-w-xs">
-                        <img src="https://mdbcdn.b-cdn.net/img/new/fluid/city/113.webp" className="max-w-xs" alt="Louvre" />
+                        <img src="https://i.ibb.co/fSkLx14/marrige.webp" className="max-w-xs" alt="Louvre" />
 
-                        <div className="absolute top-0 right-0 bottom-0 left-0 w-full h-full overflow-hidden bg-fixed opacity-0 hover:opacity-100 transition duration-300 ease-in-out bg-[#BAD4B6] bg-opacity-40">
+                        <div className="absolute top-0 right-0 bottom-0 left-0 w-full h-full overflow-hidden bg-fixed opacity-0 hover:opacity-100 transition duration-300 ease-in-out bg-green-700 bg-opacity-40">
 
                             <div className='text-white grid items-end justify-center h-full'>
-                                <h1 className='text-2xl'>Marrige</h1>
-                                <p>description:</p>
+                                <h1 className='text-5xl uppercase text-center'>Marrige</h1>
 
                                 <button className='btn-selection type-1'>Full Details</button>
 
@@ -23,13 +24,12 @@ function OurServices() {
 
                 <div className="mb-4">
                     <div className="relative overflow-hidden bg-no-repeat bg-cover max-w-xs">
-                        <img src="https://mdbcdn.b-cdn.net/img/new/fluid/city/113.webp" className="max-w-xs" alt="Louvre" />
+                        <img src="https://i.ibb.co/vqZ2qKw/fashion.webp" className="max-w-xs h-[213px]" alt="Louvre" />
 
                         <div className="absolute top-0 right-0 bottom-0 left-0 w-full h-full overflow-hidden bg-fixed opacity-0 hover:opacity-100 transition duration-300 ease-in-out bg-green-700 bg-opacity-40">
 
                             <div className='text-white grid items-end justify-center h-full'>
-                                <h1 className='text-2xl'>Corporate Event</h1>
-                                <p>description:</p>
+                                <h1 className='text-5xl uppercase text-center'>fashion</h1>
 
                                 <button className='btn-selection type-1'>Full Details</button>
 
@@ -40,13 +40,12 @@ function OurServices() {
 
                 <div className="mb-4">
                     <div className="relative overflow-hidden bg-no-repeat bg-cover max-w-xs">
-                        <img src="https://mdbcdn.b-cdn.net/img/new/fluid/city/113.webp" className="max-w-xs" alt="Louvre" />
+                        <img src="https://i.ibb.co/t2pj2VC/iftar.jpg" className="max-w-xs h-[215px]" alt="Louvre" />
 
                         <div className="absolute top-0 right-0 bottom-0 left-0 w-full h-full overflow-hidden bg-fixed opacity-0 hover:opacity-100 transition duration-300 ease-in-out bg-green-700 bg-opacity-40">
 
                             <div className='text-white grid items-end justify-center h-full'>
-                                <h1 className='text-2xl'>Global event</h1>
-                                <p>description:</p>
+                                <h1 className='text-5xl uppercase text-center'>iftar</h1>
 
                                 <button className='btn-selection type-1'>Full Details</button>
 
@@ -57,13 +56,12 @@ function OurServices() {
 
                 <div className="mb-4">
                     <div className="relative overflow-hidden bg-no-repeat bg-cover max-w-xs">
-                        <img src="https://mdbcdn.b-cdn.net/img/new/fluid/city/113.webp" className="max-w-xs" alt="Louvre" />
+                        <img src="https://i.ibb.co/XVhFNcB/crits.jpg" className="max-w-xs" alt="Louvre" />
 
                         <div className="absolute top-0 right-0 bottom-0 left-0 w-full h-full overflow-hidden bg-fixed opacity-0 hover:opacity-100 transition duration-300 ease-in-out bg-green-700 bg-opacity-40">
-
+                      
                             <div className='text-white grid items-end justify-center h-full'>
-                                <h1 className='text-2xl'>Global event</h1>
-                                <p>description:</p>
+                                <h1 className='text-5xl uppercase text-center'>christmas</h1>
 
                                 <button className='btn-selection type-1'>Full Details</button>
 
@@ -74,13 +72,12 @@ function OurServices() {
 
                 <div className="mb-4">
                     <div className="relative overflow-hidden bg-no-repeat bg-cover max-w-xs">
-                        <img src="https://mdbcdn.b-cdn.net/img/new/fluid/city/113.webp" className="max-w-xs" alt="Louvre" />
+                        <img src="https://i.ibb.co/2v1Sg87/Birthday.jpg" className="max-w-xs h-[215px] w-[400px]" alt="Louvre" />
 
                         <div className="absolute top-0 right-0 bottom-0 left-0 w-full h-full overflow-hidden bg-fixed opacity-0 hover:opacity-100 transition duration-300 ease-in-out bg-green-700 bg-opacity-40">
-
+                      
                             <div className='text-white grid items-end justify-center h-full'>
-                                <h1 className='text-2xl'>Global event</h1>
-                                <p>description:</p>
+                                <h1 className='text-5xl uppercase text-center'>birthday</h1>
 
                                 <button className='btn-selection type-1'>Full Details</button>
 
@@ -91,13 +88,12 @@ function OurServices() {
 
                 <div className="mb-4">
                     <div className="relative overflow-hidden bg-no-repeat bg-cover max-w-xs">
-                        <img src="https://mdbcdn.b-cdn.net/img/new/fluid/city/113.webp" className="max-w-xs" alt="Louvre" />
+                        <img src="https://i.ibb.co/M7L8JXM/eid.jpg" className="max-w-xs h-[215px] w-[400px]" alt="Louvre" />
 
                         <div className="absolute top-0 right-0 bottom-0 left-0 w-full h-full overflow-hidden bg-fixed opacity-0 hover:opacity-100 transition duration-300 ease-in-out bg-green-700 bg-opacity-40">
-
+                      
                             <div className='text-white grid items-end justify-center h-full'>
-                                <h1 className='text-2xl'>Global event</h1>
-                                <p>description:</p>
+                                <h1 className='text-5xl uppercase text-center'>Eid Event</h1>
 
                                 <button className='btn-selection type-1'>Full Details</button>
 
