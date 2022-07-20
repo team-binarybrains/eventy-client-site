330cfbb662eeecf784125ad115cb395a1ac94006
NEYAZ NAFIZ <niiyazobug@gmail.com>
2022-07-20 16:55:17 +0600

    sponser added in home

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,11 +1,12 @@
 import React from "react";
 import Gallery from "./Gallery/Gallery";
+import Sponser from "./Sponser/Sponser"
 
 const Home = () => {
   return (
     <div>
       <Gallery></Gallery>
-      
+      <Sponser></Sponser>
     </div>
   );
 };
