2bb51ba9851253525334b9bbfad225f4634dec3c
NEYAZ NAFIZ <niiyazobug@gmail.com>
2022-07-20 16:48:15 +0600

    gallery edited

--- src/Components/Home/Gallery/Gallery.js
+++ src/Components/Home/Gallery/Gallery.js
@@ -5,6 +5,9 @@ function Gallery() {
         <div>
             <section className="overflow-hidden text-gray-700">
                 <div className="container px-5 py-2 mx-auto lg:pt-24 lg:px-32">
+                <div className='my-4'>
+                    <h1 className='text-4xl font-serif'>Gallery</h1>
+                </div>
                     <div className="flex flex-wrap -m-1 md:-m-2">
                         <div className="flex flex-wrap w-1/2">
 
