76f24882aa727ee2499c9e1369e625aacb7f2e92
zubayer-salehin <freelancerzubear@gmail.com>
2022-07-20 17:40:44 +0600

    testimonail section create

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Gallery from "./Gallery/Gallery";
 import Sponser from "./Sponser/Sponser"
+import Testimonail from "./Testimonial/Testimonial";
 
 const Home = () => {
   return (
     <div>
       <Gallery></Gallery>
       <Sponser></Sponser>
+      <Testimonail></Testimonail>
     </div>
   );
 };
