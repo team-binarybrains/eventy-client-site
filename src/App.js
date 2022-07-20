73245b7675789b8aa0c14141f5ceda5025ca6694
MK-Sohan <mksohan786@gmail.com>
2022-07-20 16:01:47 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Navebar from "./Components/Share/Navebar";
+import Footer from "./Components/Share/Footer";
+import { Routes, Route } from "react-router-dom";
+import Home from "./Components/Home/Home";
+import NotFound from "./Components/Share/NotFound";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navebar></Navebar>
+      <Routes>
+        <Route path="/" element={<Home></Home>}></Route>
+        <Route path="*" element={<NotFound></NotFound>}></Route>
+      </Routes>
+      <Footer></Footer>
     </div>
   );
 }
---
+++ src/Components/Share/Navebar.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Navebar = () => {
+  return (
+    <div>
+      <h1>this is navebar</h1>
+    </div>
+  );
+};
+
+export default Navebar;
