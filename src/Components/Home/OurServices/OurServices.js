6fd968babf4638da8bec66a92a4a75a7ae6d268b
NEYAZ NAFIZ <niiyazobug@gmail.com>
2022-07-21 12:26:01 +0600

    animation changed

--- src/Components/Home/OurServices/OurServices.js
+++ src/Components/Home/OurServices/OurServices.js
@@ -15,7 +15,7 @@ function OurServices() {
                     <div className="relative overflow-hidden bg-no-repeat bg-cover max-w-xs">
                         <img src="https://i.ibb.co/fSkLx14/marrige.webp" className="max-w-xs" alt="Louvre" />
 
-                        <div className="absolute top-0 right-0 bottom-0 left-0 w-full h-full overflow-hidden bg-fixed opacity-0 hover:opacity-100 transition duration-300 ease-in-out bg-green-700 bg-opacity-40">
+                        <div className="absolute top-0 right-0 bottom-0 left-0 w-full h-full overflow-hidden bg-fixed opacity-0 hover:opacity-100 transition-all duration-500 ease-in-out bg-green-700 bg-opacity-40">
 
                             <div className='text-white grid items-end justify-center h-full'>
                                 <h1 className='text-5xl uppercase text-center text-white'>Marrige</h1>
@@ -31,7 +31,7 @@ function OurServices() {
                     <div className="relative overflow-hidden bg-no-repeat bg-cover max-w-xs">
                         <img src="https://i.ibb.co/vqZ2qKw/fashion.webp" className="max-w-xs h-[213px]" alt="Louvre" />
 
-                        <div className="absolute top-0 right-0 bottom-0 left-0 w-full h-full overflow-hidden bg-fixed opacity-0 hover:opacity-100 transition duration-300 ease-in-out bg-green-700 bg-opacity-40">
+                        <div className="absolute top-0 right-0 bottom-0 left-0 w-full h-full overflow-hidden bg-fixed opacity-0 hover:opacity-100 transition-all duration-500 ease-in-out bg-green-700 bg-opacity-40">
 
                             <div className='text-white grid items-end justify-center h-full'>
                                 <h1 className='text-5xl uppercase text-center text-white'>fashion</h1>
@@ -47,7 +47,7 @@ function OurServices() {
                     <div className="relative overflow-hidden bg-no-repeat bg-cover max-w-xs">
                         <img src="https://i.ibb.co/t2pj2VC/iftar.jpg" className="max-w-xs h-[215px]" alt="Louvre" />
 
-                        <div className="absolute top-0 right-0 bottom-0 left-0 w-full h-full overflow-hidden bg-fixed opacity-0 hover:opacity-100 transition duration-300 ease-in-out bg-green-700 bg-opacity-40">
+                        <div className="absolute top-0 right-0 bottom-0 left-0 w-full h-full overflow-hidden bg-fixed opacity-0 hover:opacity-100 transition-all duration-500 ease-in-out bg-green-700 bg-opacity-40">
 
                             <div className='text-white grid items-end justify-center h-full'>
                                 <h1 className='text-5xl uppercase text-center text-white'>iftar</h1>
@@ -63,7 +63,7 @@ function OurServices() {
                     <div className="relative overflow-hidden bg-no-repeat bg-cover max-w-xs">
                         <img src="https://i.ibb.co/XVhFNcB/crits.jpg" className="max-w-xs" alt="Louvre" />
 
-                        <div className="absolute top-0 right-0 bottom-0 left-0 w-full h-full overflow-hidden bg-fixed opacity-0 hover:opacity-100 transition duration-300 ease-in-out bg-green-700 bg-opacity-40">
+                        <div className="absolute top-0 right-0 bottom-0 left-0 w-full h-full overflow-hidden bg-fixed opacity-0 hover:opacity-100 transition-all duration-500 ease-in-out bg-green-700 bg-opacity-40">
 
                             <div className='text-white grid items-end justify-center h-full'>
                                 <h1 className='text-5xl uppercase text-center text-white'>christmas</h1>
@@ -79,7 +79,7 @@ function OurServices() {
                     <div className="relative overflow-hidden bg-no-repeat bg-cover max-w-xs">
                         <img src="https://i.ibb.co/2v1Sg87/Birthday.jpg" className="max-w-xs h-[215px] w-[400px]" alt="Louvre" />
 
-                        <div className="absolute top-0 right-0 bottom-0 left-0 w-full h-full overflow-hidden bg-fixed opacity-0 hover:opacity-100 transition duration-300 ease-in-out bg-green-700 bg-opacity-40">
+                        <div className="absolute top-0 right-0 bottom-0 left-0 w-full h-full overflow-hidden bg-fixed opacity-0 hover:opacity-100 transition-all duration-500 ease-in-out bg-green-700 bg-opacity-40">
 
                             <div className='text-white grid items-end justify-center h-full'>
                                 <h1 className='text-5xl uppercase text-center text-white'>birthday</h1>
@@ -95,7 +95,7 @@ function OurServices() {
                     <div className="relative overflow-hidden bg-no-repeat bg-cover max-w-xs">
                         <img src="https://i.ibb.co/M7L8JXM/eid.jpg" className="max-w-xs h-[215px] w-[400px]" alt="Louvre" />
 
-                        <div className="absolute top-0 right-0 bottom-0 left-0 w-full h-full overflow-hidden bg-fixed opacity-0 hover:opacity-100 transition duration-300 ease-in-out bg-green-700 bg-opacity-40">
+                        <div className="absolute top-0 right-0 bottom-0 left-0 w-full h-full overflow-hidden bg-fixed opacity-0 hover:opacity-100 transition-all duration-500 ease-in-out bg-green-700 bg-opacity-40">
 
                             <div className='text-white grid items-end justify-center h-full'>
                                 <h1 className='text-5xl uppercase text-center text-white'>Eid Event</h1>
