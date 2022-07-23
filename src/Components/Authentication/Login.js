8f1f6d7f5fbbc4a8a3d4422cdbc3ea9101f0c98a
MK-Sohan <mksohan786@gmail.com>
2022-07-24 00:34:21 +0600

    login modal implement

--- src/Components/Authentication/Login.js
+++ src/Components/Authentication/Login.js
@@ -3,7 +3,29 @@ import React from "react";
 const Login = () => {
   return (
     <div>
-      <h1>This is login section</h1>
+      <input type="checkbox" id="login-modal" class="modal-toggle" />
+      <div class="modal ">
+        <div class="modal-box rounded-md">
+          <div className="flex justify-between">
+            <h1 class="font-bold text-lg text-center ">Login</h1>
+            <label
+              for="login-modal"
+              class="text-white p-2 w-10 text-center hover:bg-black font-bold cursor-pointer rounded-full  bg-green-400 border-none "
+            >
+              X
+            </label>
+          </div>
+
+          <div class="modal-action  flex justify-center">
+            <button
+              for="login-modal"
+              class="  btn bg-green-400 border-none w-36 "
+            >
+              Login
+            </button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
