9afb711e0d2adca4bbb81a65ca73e3b4c4124dcd
NEYAZ NAFIZ <niiyazobug@gmail.com>
2022-07-24 03:26:10 +0600

    footer edited

--- src/Components/Home/Sponser/Sponser.js
+++ src/Components/Home/Sponser/Sponser.js
@@ -3,36 +3,36 @@ import React, { useEffect, useState } from 'react';
 import './sponser.css'
 
 const Sponser = () => {
-    const [sponsers,setSponsers]= useState([]);
+    const [sponsers, setSponsers] = useState([]);
 
-    useEffect(()=> {
+    useEffect(() => {
         fetch('sponsers.json')
-        .then(response => response.json())
-        .then(data => setSponsers(data))
-    },[])
+            .then(response => response.json())
+            .then(data => setSponsers(data))
+    }, [])
 
     return (
         <div>
-            <section className="bg-tertiary py-12">
-            <div className="container mx-auto space-y-3">
-                <h1 className='text-center col-span-full text-4xl title_line uppercase'>Sponsers</h1>
-                <div className="grid grid-cols-4 md:grid-cols-8 md:grid-flow-col px-5 sm:px-10 py-10 gap-x-2 gap-y-5 my-shadow">
-                    {sponsers.map((sponser, index) => {
-                        return (
-                            <div
-                                data-aos="zoom-in-down"
-                                data-aos-easing="linear"
-                                data-aos-duration="1000"
-                                className="flex items-center justify-center"
-                                key={index}
-                            >
-                                <img className="" src={sponser.img} alt="" />
-                            </div>
-                        );
-                    })}
+            <section className="bg-tertiary py-12 px-2">
+                <div className="container mx-auto space-y-3">
+                    <h1 className='text-center col-span-full text-4xl title_line uppercase'>Sponsers</h1>
+                    <div className="grid grid-cols-4 md:grid-cols-8 md:grid-flow-col px-5 sm:px-10 py-10 gap-x-2 gap-y-5 my-shadow">
+                        {sponsers.map((sponser, index) => {
+                            return (
+                                <div
+                                    data-aos="zoom-in-down"
+                                    data-aos-easing="linear"
+                                    data-aos-duration="1000"
+                                    className="flex items-center justify-center"
+                                    key={index}
+                                >
+                                    <img className="" src={sponser.img} alt="" />
+                                </div>
+                            );
+                        })}
+                    </div>
                 </div>
-            </div>
-        </section>
+            </section>
         </div>
     );
 };
