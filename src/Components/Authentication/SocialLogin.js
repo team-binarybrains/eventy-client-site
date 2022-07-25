84ce68ae7bcd103478ce547dbc6132bb163d9f7e
NEYAZ NAFIZ <niiyazobug@gmail.com>
2022-07-25 14:23:59 +0600

    social authenticatin done

--- src/Components/Authentication/SocialLogin.js
+++ src/Components/Authentication/SocialLogin.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import auth from '../../Firebase/Firebase.init'
 import { useSignInWithFacebook, useSignInWithGoogle, useSignInWithTwitter } from 'react-firebase-hooks/auth';
-import { AiFillGoogleCircle } from 'react-icons/ai';
+import { FcGoogle } from 'react-icons/fc';
 import { BsFacebook } from 'react-icons/bs';
 import { useLocation, useNavigate } from 'react-router-dom';
 // import Loading from '../../../Shared/Loading/Loading';
 // import { toast, ToastContainer } from 'react-toastify';
 // import 'react-toastify/dist/ReactToastify.css';
 // import axios from 'axios';
+import '../ContactUs/ContactUs.css'
 
 
 const SocialLogin = () => {
@@ -47,13 +48,13 @@ const SocialLogin = () => {
       <div className=' py-2 px-4 flex lg:justify-between justify-center lg:gap-0 gap-16 items-center social-shadow'>
 
         <div className='text-gray-300 hover:text-gray-800'>
-          <button onClick={() => signInWithGoogle()} className='lg:text-4xl text-6xl'> <AiFillGoogleCircle className=''></AiFillGoogleCircle></button>
+          <button onClick={() => signInWithGoogle()} className='lg:text-4xl text-6xl hover:opacity-60 bounce'> <FcGoogle className=''></FcGoogle></button>
         </div>
 
         <h2 className='font-semibold hidden lg:block'>CONNECT WITH SOCIAL ACCOUNT</h2>
 
         <div className='text-gray-300 hover:text-gray-800'>
-          <button onClick={() => signInWithFacebook()} className='lg:text-3xl text-5xl'> <BsFacebook className=''></BsFacebook></button>
+          <button onClick={() => signInWithFacebook()} className='lg:text-3xl text-5xl hover:opacity-60 bounce '> <BsFacebook className='bg-white text-sky-700'></BsFacebook></button>
         </div>
 
       </div>
