626893ccc4541b95e57dad733cf53b075a6d56d2
NEYAZ NAFIZ <niiyazobug@gmail.com>
2022-07-25 15:30:39 +0600

    image div edited

--- src/Components/AboutUs/AboutUs.js
+++ src/Components/AboutUs/AboutUs.js
@@ -20,8 +20,23 @@ const AboutUs = () => {
           <p className="font-normal text-base leading-6 text-gray-600 ">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum.In the first place we have granted to God, and by this our present charter confirmed for us and our heirs forever that the English Church shall be free, and shall have her rights entire, and her liberties inviolate; and we will that it be thus observed; which is apparent from</p>
         </div>
         <div className="w-full lg:w-8/12 lg:pt-8">
-          <div className="grid md:grid-cols-4 sm:grid-cols-2 grid-cols-1 lg:gap-4 shadow-lg rounded-md ">
+          <div className="shadow-lg rounded-md ">
+
+          <div className="grid md:grid-cols-2 sm:grid-cols-2 grid-cols-1 lg:gap-4 ">
+           <div className="p-4 pb-6 flex justify-center flex-col items-center">
+              <img className="md:block hidden" src="https://i.ibb.co/7nSJPXQ/Rectangle-121.png" alt="Elijah featured img" />
+              <img className="md:hidden block" src="https://i.ibb.co/ThZBWxH/Rectangle-121.png" alt="Elijah featured img" />
+              <p className="font-medium text-xl leading-5 text-gray-800 mt-4">Elijah</p>
+            </div>
             <div className="p-4 pb-6 flex justify-center flex-col items-center">
+              <img className="md:block hidden" src="https://i.ibb.co/7nSJPXQ/Rectangle-121.png" alt="Elijah featured img" />
+              <img className="md:hidden block" src="https://i.ibb.co/ThZBWxH/Rectangle-121.png" alt="Elijah featured img" />
+              <p className="font-medium text-xl leading-5 text-gray-800 mt-4">Elijah</p>
+            </div>
+           </div>
+           
+           <div className="grid md:grid-cols-4 sm:grid-cols-2 grid-cols-1 lg:gap-4">
+           <div className="p-4 pb-6 flex justify-center flex-col items-center">
               <img className="md:block hidden" src="https://i.ibb.co/FYTKDG6/Rectangle-118-2.png" alt="Alexa featured Img" />
               <img className="md:hidden block" src="https://i.ibb.co/zHjXqg4/Rectangle-118.png" alt="Alexa featured Img" />
               <p className="font-medium text-xl leading-5 text-gray-800 mt-4">Alexa</p>
@@ -41,16 +56,7 @@ const AboutUs = () => {
               <img className="md:hidden block" src="https://i.ibb.co/ThZBWxH/Rectangle-121.png" alt="Elijah featured img" />
               <p className="font-medium text-xl leading-5 text-gray-800 mt-4">Elijah</p>
             </div>
-            <div className="p-4 pb-6 flex justify-center flex-col items-center">
-              <img className="md:block hidden" src="https://i.ibb.co/7nSJPXQ/Rectangle-121.png" alt="Elijah featured img" />
-              <img className="md:hidden block" src="https://i.ibb.co/ThZBWxH/Rectangle-121.png" alt="Elijah featured img" />
-              <p className="font-medium text-xl leading-5 text-gray-800 mt-4">Elijah</p>
-            </div>
-            <div className="p-4 pb-6 flex justify-center flex-col items-center">
-              <img className="md:block hidden" src="https://i.ibb.co/7nSJPXQ/Rectangle-121.png" alt="Elijah featured img" />
-              <img className="md:hidden block" src="https://i.ibb.co/ThZBWxH/Rectangle-121.png" alt="Elijah featured img" />
-              <p className="font-medium text-xl leading-5 text-gray-800 mt-4">Elijah</p>
-            </div>
+           </div>
           </div>
         </div>
       </div>
