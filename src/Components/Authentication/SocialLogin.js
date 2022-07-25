461f09868f4a98b5175aaba88b6e080fcb5ebbca
NEYAZ NAFIZ <niiyazobug@gmail.com>
2022-07-25 13:55:55 +0600

    social authentication added

---
+++ src/Components/Authentication/SocialLogin.js
@@ -0,0 +1,65 @@
+import React from 'react';
+import auth from '../../Firebase/Firebase.init'
+import { useSignInWithFacebook, useSignInWithGoogle, useSignInWithTwitter } from 'react-firebase-hooks/auth';
+import { AiFillGoogleCircle } from 'react-icons/ai';
+import { BsFacebook } from 'react-icons/bs';
+import { useLocation, useNavigate } from 'react-router-dom';
+// import Loading from '../../../Shared/Loading/Loading';
+// import { toast, ToastContainer } from 'react-toastify';
+// import 'react-toastify/dist/ReactToastify.css';
+// import axios from 'axios';
+
+
+const SocialLogin = () => {
+
+
+  const [signInWithGoogle, googleUser, googleLoading, googleError] = useSignInWithGoogle(auth)
+
+  const [signInWithFacebook, facebookUser, FacebookLoading, FacebookError] = useSignInWithFacebook(auth);
+
+  const navigate = useNavigate()
+  const location = useLocation()
+  let from = location.state?.from?.pathname || "/";
+
+  if (googleError || FacebookError) {
+
+    // toast.error(<>{googleError?.message}, {FacebookError?.message}</>)
+  }
+
+  if (googleLoading || FacebookLoading) {
+    // return <Loading></Loading>
+  }
+
+  if (googleUser || facebookUser) {
+    navigate(from, { replace: true });
+
+    // toast.success('Congratulation ! You are Loged In successfully. Enjoy our more feature here.')
+  }
+
+  // const googleSignIn = async () => {
+  //   signInWithGoogle()
+  //   const { data } = await axios.post(`https://cryptic-stream-11517.herokuapp.com/signup`, {googleUser})
+  //       localStorage.setItem('accessJwtToken', data.accessJwtToken)
+  // }
+
+  return (
+    <div>
+      <div className=' py-2 px-4 flex lg:justify-between justify-center lg:gap-0 gap-16 items-center social-shadow'>
+
+        <div className='text-gray-300 hover:text-gray-800'>
+          <button onClick={() => signInWithGoogle()} className='lg:text-4xl text-6xl'> <AiFillGoogleCircle className=''></AiFillGoogleCircle></button>
+        </div>
+
+        <h2 className='font-semibold hidden lg:block'>CONNECT WITH SOCIAL ACCOUNT</h2>
+
+        <div className='text-gray-300 hover:text-gray-800'>
+          <button onClick={() => signInWithFacebook()} className='lg:text-3xl text-5xl'> <BsFacebook className=''></BsFacebook></button>
+        </div>
+
+      </div>
+
+    </div>
+  );
+};
+
+export default SocialLogin;
