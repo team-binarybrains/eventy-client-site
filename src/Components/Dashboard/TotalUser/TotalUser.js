58b985bc47d9b9d7edcf7f03fd32c5ce865a0f2d
neyaznafiz <niiyazobug@gmail.com>
2022-07-27 18:36:48 +0600

    all use data fetch

--- src/Components/Dashboard/TotalUser/TotalUser.js
+++ src/Components/Dashboard/TotalUser/TotalUser.js
@@ -1,6 +1,20 @@
 import React from 'react';
+import { useState } from 'react'
+import axios from 'axios'
+
 
 const TotalUser = () => {
+
+   const [allUser, setAllUser] = useState([])
+
+   axios.get('http://localhost:5000/allusers')
+   .then(res => {
+      const { data } = res
+      setAllUser(data);
+      console.log(data);
+   })
+
+
    return (
       <div>
          <h1>Total user</h1>
