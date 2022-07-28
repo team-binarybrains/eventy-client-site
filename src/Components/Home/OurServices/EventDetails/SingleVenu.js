bcb00418d690cbfc924ab9cfd627a2ad7fc09836
zubayer-salehin <freelancerzubear@gmail.com>
2022-07-28 16:37:16 +0600

    eventDetailspage working

--- src/Components/Home/OurServices/EventDetails/SingleVenu.js
+++ src/Components/Home/OurServices/EventDetails/SingleVenu.js
@@ -15,7 +15,7 @@ const SingleVenu = ({ VenuDetails, index, handleSelectVenu, selectVenu }) => {
                 />
                 <div className="p-3">
                     <p className="text-lg"><b>Event Location :</b> {location}</p>
-                    <p className="text-base pt-1"><b>Price :</b> {price}</p>
+                    <p className="text-base pt-1"><b>Price :</b> ${price}</p>
                     <p className="text-base pt-1"><b>Quantity :</b> {quantity} peoples</p>
                     <p className="w-full pt-1"><b>Details : </b> {description.slice(0, 190)}...</p>
                     <button onClick={() => handleSelectVenu(VenuDetails)} className="bg-gray-800 text-white mt-3 px-10 py-2">Select Venu</button>
