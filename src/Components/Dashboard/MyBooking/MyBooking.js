a99c0942ea39e0cbcdad852f863ef50ed31227a1
developerabdullah-me <abdullahtalha9999@gmail.com>
2022-07-28 19:51:24 +0600

    my booking

--- src/Components/Dashboard/MyBooking/MyBooking.js
+++ src/Components/Dashboard/MyBooking/MyBooking.js
@@ -3,7 +3,32 @@ import React from 'react';
 const MyBooking = () => {
    return (
       <div>
-         <h1>this my booking</h1>
+        
+        <h1>my order </h1>
+
+<div className="overflow-x-auto">
+  <table className="table table-zebra w-full">
+    <thead>
+      <tr>
+        {/* <th>SL</th> */}
+        <th className="text-xl font-bold">Details</th>
+
+        <th className="text-xl font-bold">Payment</th>
+        <th className="text-xl font-bold">cancel</th>
+      </tr>
+    </thead>
+
+    <tbody>
+      {/* {items?.map((user) => (
+        <SingleMyOrder
+          key={user._id}
+          user={user}
+          productDeleteHandle={productDeleteHandle}
+        ></SingleMyOrder>
+      ))} */}
+    </tbody>
+  </table>
+</div>
       </div>
    );
 };
---
+++ src/Components/Dashboard/MyBooking/SinglMyBooking/SinglMyBooking.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const SingleMyBooking = () => {
+    return (
+        <div>
+            
+            
+        </div>
+    );
+};
+
+export default SingleMyBooking;
