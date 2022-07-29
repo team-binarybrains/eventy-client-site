48de063a5272be436a90524a101939876b65a365
developerabdullah-me <abdullahtalha9999@gmail.com>
2022-07-29 15:28:41 +0600

    my booking final

--- src/Components/Dashboard/MyBooking/MyBooking.js
+++ src/Components/Dashboard/MyBooking/MyBooking.js
@@ -1,57 +1,55 @@
-import { signOut } from 'firebase/auth';
+import { signOut } from "firebase/auth";
 import axios from "axios";
-import React, { useEffect, useState } from 'react';
-import { useAuthState } from 'react-firebase-hooks/auth';
-import { useNavigate } from 'react-router-dom';
-import auth from '../../../Firebase/Firebase.init';
-import SingleMyBooking from './SinglMyBooking/SinglMyBooking';
+import React, { useEffect, useState } from "react";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { useNavigate } from "react-router-dom";
+import auth from "../../../Firebase/Firebase.init";
+import SingleMyBooking from "./SinglMyBooking/SinglMyBooking";
 
 const MyBooking = () => {
-   const [user] = useAuthState(auth);
-   const [items, setItems] = useState([]);
-   const navigate = useNavigate();
-useEffect(() => {
-   const email=user.email;
-   const url = `http://localhost:5000/myBooking?email=${email}`;
-   fetch(url)
-   .then(res=> res.json())
-   .then(data=> setItems(data))
-},[])
+  const [user] = useAuthState(auth);
+  const [items, setItems] = useState([]);
 
-const productDeleteHandle = (id) => {
-   const proceed = window.confirm("Are you sure cancel booking?");
-   if (proceed) {
-     const url = `http://localhost:5000/myBooking/${id}`;
-     fetch(url, {
-       method: "DELETE",
-     })
-       .then((res) => res.json())
-       .then((data) => {
-         const remaining = items.filter(
-           (OrderItems) => OrderItems._id !== id
-         );
-         setItems(remaining);
-       });
-   }
- };
+  useEffect(() => {
+    const email = user.email;
+    const url = `http://localhost:5000/myBooking/${email}`;
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => setItems(data));
+  }, [user]);
 
-   return (
-      <div>
-        
-        <h1 className='pt-3 pb-3 '>my order {items.length}</h1>
+  const productDeleteHandle = (id) => {
+    const proceed = window.confirm("Are you sure cancel booking?");
+    if (proceed) {
+      const url = `http://localhost:5000/myBooking/${id}`;
+      fetch(url, {
+        method: "DELETE",
+      })
+        .then((res) => res.json())
+        .then((data) => {
+          const remaining = items.filter((OrderItems) => OrderItems._id !== id);
+          setItems(remaining);
+        });
+    }
+  };
 
-<div className="grid md:grid-cols-2 gap-7 sm:grid-cols-1 px-3 pt-7">
-{items?.map(user => (
-        <SingleMyBooking
-          key={user._id}
-          user={user}
-          productDeleteHandle={productDeleteHandle}
-        ></SingleMyBooking>
-      ))}
-      
-</div>
+  return (
+    <div>
+      <h1 className="pt-3 pb-3  text-3xl mt-4 uppercase ml-5">
+      Total order: {items.length}
+      </h1>
+
+      <div className="grid md:grid-cols-2 gap-7 sm:grid-cols-1 px-3 pt-7">
+        {items?.map((user) => (
+          <SingleMyBooking
+            key={user._id}
+            user={user}
+            productDeleteHandle={productDeleteHandle}
+          ></SingleMyBooking>
+        ))}
       </div>
-   );
+    </div>
+  );
 };
 
 export default MyBooking;
